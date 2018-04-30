1720896f4984629c32ca70e60daabf0974ef195b
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-30 14:24:57 -0400

    Fixed test script, cleaned up examples directory

--- README.md
+++ README.md
@@ -11,4 +11,11 @@ hURL is a file transfer library supporting HTTP protocol, inspired by the cURL c
 - do `cmake ..`, specifying any include paths for Asio, Boost, or OpenSSL (usually not necessary if your PATH environment variable is configured already.
 - `make`
 
+## Running the tests
+- Go to `build` directory.
+- Run make to compile examples and tests. The executables will be placed in `build/bin`
+- To run a test comparing the Easy handle with a single threaded Multi handle, run `./bin/easy_vs_single_multi`. The results of this test will be in a csv file in `build/results`.
+- To run a whole bunch of executables comparing hURL and cURL, run `../tests/test.sh`.
+This will download a bunch of files placing them in `build/hURLed` and write the timing results to specific csv files found in `build/results`.
+
 NOTE: we have not yet resolved dependency issues with OpenSSL, so it is highly likely that you will not be able to build the ssl examples, or the ssl handshake will fail.
--- tests/test.sh
+++ tests/test.sh
@@ -3,55 +3,55 @@
 echo image_DP87 HTTPS 219K
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url https://images.metmuseum.org/CRDImages/dp/web-large/DP875552.jpg >  build/results/DP87.jpg
-    ./build/bin/simple_c_url https://images.metmuseum.org/CRDImages/dp/web-large/DP875552.jpg > build/results/DP87c.jpg
+    bin/simple_url https://images.metmuseum.org/CRDImages/dp/web-large/DP875552.jpg >  hURLed/DP87.jpg
+    bin/simple_c_url https://images.metmuseum.org/CRDImages/dp/web-large/DP875552.jpg > hURLed/DP87c.jpg
 done
 
 echo Happy Image HTTPS 69K
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png> build/results/happy.png
-    ./build/bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png > build/results/happy_c.png
+    bin/simple_url https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png> hURLed/happy.png
+    bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png > hURLed/happy_c.png
 done
 
 echo HTTP ~23K
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url http://www.stroustrup.com/Bjarne2018.jpg > build/results/Bjarne.jpg
-    ./build/bin/simple_c_url http://www.stroustrup.com/Bjarne2018.jpg > build/results/Bjarne_c.jpg
+    bin/simple_url http://www.stroustrup.com/Bjarne2018.jpg > hURLed/Bjarne.jpg
+    bin/simple_c_url http://www.stroustrup.com/Bjarne2018.jpg > hURLed/Bjarne_c.jpg
 done
 
 echo HTTP ~1MB
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url http://images.cocodataset.org/annotations/image_info_test2014.zip > build/results/coco_anotations.zip
-    ./build/bin/simple_c_url http://images.cocodataset.org/annotations/image_info_test2014.zip > build/results/coco_anotations_c.zip
+    bin/simple_url http://images.cocodataset.org/annotations/image_info_test2014.zip > hURLed/coco_anotations.zip
+    bin/simple_c_url http://images.cocodataset.org/annotations/image_info_test2014.zip > hURLed/coco_anotations_c.zip
 done
 
 echo HTTPS 222K stephen hawking
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg/1599px-Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg > build/results/stephen.jpg
-    ./build/bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg/1599px-Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg > build/results/stephen_c.jpg
+    bin/simple_url https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg/1599px-Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg > hURLed/stephen.jpg
+    bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg/1599px-Physicist_Stephen_Hawking_in_Zero_Gravity_NASA.jpg > hURLed/stephen_c.jpg
 done
 
 echo HTTPS 14M Video firetruck
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url https://upload.wikimedia.org/wikipedia/commons/transcoded/3/3d/Fire_training_alarm_in_KarRC_RAS_2018.ogv/Fire_training_alarm_in_KarRC_RAS_2018.ogv.480p.webm > build/results/firetruck.webm
-    ./build/bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/transcoded/3/3d/Fire_training_alarm_in_KarRC_RAS_2018.ogv/Fire_training_alarm_in_KarRC_RAS_2018.ogv.480p.webm > build/results/firetruck_c.webm
+    bin/simple_url https://upload.wikimedia.org/wikipedia/commons/transcoded/3/3d/Fire_training_alarm_in_KarRC_RAS_2018.ogv/Fire_training_alarm_in_KarRC_RAS_2018.ogv.480p.webm > hURLed/firetruck.webm
+    bin/simple_c_url https://upload.wikimedia.org/wikipedia/commons/transcoded/3/3d/Fire_training_alarm_in_KarRC_RAS_2018.ogv/Fire_training_alarm_in_KarRC_RAS_2018.ogv.480p.webm > hURLed/firetruck_c.webm
 done
 
 
 for i in `seq 1 20`;
 do
-    ./build/bin/simple_url http://images.cocodataset.org/zips/val2017.zip > build/results/val2017.zip
-    ./build/bin/simple_c_url http://images.cocodataset.org/zips/val2017.zip > build/results/val2017_c.zip
+    bin/simple_url http://images.cocodataset.org/zips/val2017.zip > hURLed/val2017.zip
+    bin/simple_c_url http://images.cocodataset.org/zips/val2017.zip > hURLed/val2017_c.zip
 done
 
 #echo Large files ~6GB
 #for i in `seq 1 20`;
 #do
-    #./build/bin/simple_url http://images.cocodataset.org/zips/test2014.zip > build/results/coco.zip
-    #./build/bin/simple_c_url http://images.cocodataset.org/zips/test2014.zip > build/results/coco_c.zip
+    #bin/simple_url http://images.cocodataset.org/zips/test2014.zip > hURLed/coco.zip
+    #bin/simple_c_url http://images.cocodataset.org/zips/test2014.zip > hURLed/coco_c.zip
 #done
